9c0ff2766e65a115827e97d39f53e19b9405dce9
avan-web <avanthikavaru003@gmail.com>
2025-02-24 21:46:29 +0530

    Updated form

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-document.getElementById("contactForm").addEventListener("submit",async function (event) {
-    event.preventDefault(); // prevent page refresh
+// document.getElementById("contactForm").addEventListener("submit",async function (event) {
+//     event.preventDefault(); // prevent page refresh
 
-    // Get form values
-    const name = document.getElementById("name").value.trim();
-    const email = document.getElementById("email").value.trim();
-    const message = document.getElementById("message").value.trim();
+//     // Get form values
+//     const name = document.getElementById("name").value.trim();
+//     const email = document.getElementById("email").value.trim();
+//     const message = document.getElementById("message").value.trim();
 
-    // validate input fields
-    if (!name || !email || !message) {
-        alert("All fields are required");
-        return;
-    }
+//     // validate input fields
+//     if (!name || !email || !message) {
+//         alert("All fields are required");
+//         return;
+//     }
 
-    try {
-        const response = await fetch("/contact", {
-            method:"POST",
-            headers: {
-                "content-Type" : "application/json"
-            },
-            body: JSON.stringify({ name,email,message})
-        });
+//     try {
+//         const response = await fetch("/contact", {
+//             method:"POST",
+//             headers: {
+//                 "content-Type" : "application/json"
+//             },
+//             body: JSON.stringify({ name,email,message})
+//         });
          
-        const result = await response.json();
-        if (response.ok) {
-            alert ("message sent successfully!");
-        } else {
-            alert(`Error: ${result.message}`);
-        }   
-    } catch (error) {
-        console.error("Request failed:",error);
-        alert("Something went wrong.Please try again later.");
-    }
-});
+//         const result = await response.json();
+//         if (response.ok) {
+//             alert ("message sent successfully!");
+//         } else {
+//             alert(`Error: ${result.message}`);
+//         }   
+//     } catch (error) {
+//         console.error("Request failed:",error);
+//         alert("Something went wrong.Please try again later.");
+//     }
+// });
